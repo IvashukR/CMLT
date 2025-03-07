c5f849f9797a88f0d4c0a81d9917202c8ef29f8e
IvashukR <rost87085@gmail.com>
2025-03-07 13:22:47 +0200

    add remove func

--- cml_utils.sh
+++ cml_utils.sh
@@ -4,6 +4,10 @@ function show_help()
     echo "cml functions:"
     echo "cml start - start write you commands in logs"
 }
+function remove_util()
+{
+    sudo rm -rf /usr/local/bin/cml
+}
 if [ "$#" -eq 0 ]; then
     echo "not have argument, try cml help"
 fi
@@ -12,4 +16,6 @@ case $1 in
     help)
         show_help
         ;;
+    rm)
+        remove_util
 esac
