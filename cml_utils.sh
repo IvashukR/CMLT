be1519d9eaa6189aa3dba6ee4088c46d18e0bb93
IvashukR <rost87085@gmail.com>
2025-03-07 13:11:56 +0200

    test2 help commands

--- cml_utils.sh
+++ cml_utils.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-name_utilit = $0
 function show_help()
 {
     echo "cml functions:"
     echo "cml start - start write you commands in logs"
 }
-if [[$# -eg 0]]; then
-    echo "not have argument, try $name_utilit help"
-fi
+echo "not have argument, try cml help"
+
 case $1 in
     help)
         show_help
